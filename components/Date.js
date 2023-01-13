e38123c3b9faf7f84f750e02d607ff6c9786296e
hossam <hossam.salem667@gmail.com>
2023-01-13 23:16:50 +0200

    google-analytics

--- components/Date.js
+++ components/Date.js
@@ -4,6 +4,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 // import dayjs from 'dayjs';
 import TextField from "@mui/material/TextField";
+import TextInput from "./TextInput";
 
 const Date = () => {
   const [value, setValue] = React.useState("2014-08-18T21:11:54");
@@ -15,12 +16,13 @@ const Date = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DesktopDatePicker
-        label="Date desktop"
         inputFormat="MM/DD/YYYY"
         value={value}
         onChange={handleChange}
         renderInput={(params) => <TextField {...params} />}
         shouldDisableYear={(year) => false}
+        openTo="day"
+        // InputProps={{ className: { color: "red" } }}
       />
     </LocalizationProvider>
   );
