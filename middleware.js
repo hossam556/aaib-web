46c43af61922d513e4f51e44ce423fba13974235
hossam <hossam.salem667@gmail.com>
2023-02-11 20:51:58 +0200

    peotect route

---
+++ middleware.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  // const url = req.nextUrl.clone();
+  // url.pathname = "/";
+  // return NextResponse.redirect(url);
+}
